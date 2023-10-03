52bf0591c153fb99241f56eb171a40202df6195e
LucasMerino10 <lucas.merino9@gmail.com>
2023-10-03 10:32:22 +0200

    adding files

---
+++ index.js
@@ -0,0 +1,6 @@
+import chalk from 'chalk';
+
+console.log(chalk.green('Xavier'));
+console.log(chalk.blue('Mathieu'));
+console.log(chalk.yellow('Pierre Adrien'));
+console.log(chalk.red('Romain'));
